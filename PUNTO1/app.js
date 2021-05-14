cea9d20bf0960459c397dd4ca3c81d9d764c4b01
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2021-05-14 07:36:21 -0500

    Peticion archivos

--- PUNTO1/app.js
+++ PUNTO1/app.js
@@ -1,3 +1,40 @@
-const fs= require('fs');
-var files= fs.readdirSync("./assets");
-console.log(files);
+const fs=require('fs');
+const path =require('path');
+const express=require('express');
+const bp=requier('body-parser');
+
+const MarkdownIt=require('markdown-it');
+    md=new MarkdownIt;
+const app =express();
+
+app.use(express.static('pub'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+	extended: true
+})) 
+
+app.listen(3000, () => {
+	console.log("Escuchando en: http://localhost:3000")
+})
+
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'))
+})
+app.get('/listarArchivos', (request, response) => {
+	fs.readdir(path.resolve(__dirname, 'pub/'), 'utf8',
+		(err, data) => {
+			if (error) {
+				console.error(err)
+				response.status(500).json({
+					error: 'message'
+				})
+				return
+			}
+            console.log(files);
+            console.json(files);
+			response.json({
+				text: data.replace(/\n/g, '<br>')
+			})
+		})
+      //
+})
